9e1a86b1abd0282c1ee2b0aacdb31d280b24b811
yoonsangkwak <yunsang08@naver.com>
2023-06-24 18:05:55 +0900

    remote 모듈 생성

---
+++ remote/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.ddd.ansayo.remote"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+}
+
+dependencies {
+    implementation(project(":data"))
+}
