723192c1ab840297c172e0096fbca1d58f2d2c18
yoonsangkwak <yunsang08@naver.com>
2023-07-26 22:57:41 +0900

    [ feature ] : handler 로직 변경

--- domain/src/main/java/com/ddd/ansayo/domain/handler/course/CourseWriteEventHandler.kt
+++ domain/src/main/java/com/ddd/ansayo/domain/handler/course/CourseWriteEventHandler.kt
@@ -2,114 +2,146 @@ package com.ddd.ansayo.domain.handler.course
 
 import com.ddd.ansayo.domain.model.common.Response
 import com.ddd.ansayo.domain.model.course.CoursePlaceImage
-import com.ddd.ansayo.domain.model.course.CourseWriteEvent
-import com.ddd.ansayo.domain.model.course.CourseWriteSideEffect
+import com.ddd.ansayo.domain.model.course.CourseWriteMutation
+import com.ddd.ansayo.domain.model.course.CourseWriteAction
 import com.ddd.ansayo.domain.model.course.CourseWriteState
+import com.ddd.ansayo.domain.usecase.course.GetImageUploadUrlUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
-class CourseWriteEventHandler @Inject constructor() {
+class CourseWriteEventHandler @Inject constructor(
+    private val getImageUploadUrlUseCase: GetImageUploadUrlUseCase
+) {
 
-    fun reduceState(
-        currentState: CourseWriteState,
-        event: CourseWriteEvent
-    ): CourseWriteState {
-        return when (event) {
-            is CourseWriteEvent.InputCourseTitle -> {
-                currentState.copy(isCourseTitleMaxInputted = event.text.length == COURSE_TITLE_MAX_LENGTH)
+    suspend fun mutate(
+        state: CourseWriteState,
+        action: CourseWriteAction
+    ): Flow<CourseWriteMutation> {
+        return when (action) {
+            is CourseWriteAction.InputCourseTitle -> {
+                flowOf(
+                    CourseWriteMutation.Mutation.UpdateCourseTitle(
+                        isCourseTitleMaxInputted = action.text.length == COURSE_TITLE_MAX_LENGTH
+                    )
+                )
             }
-            is CourseWriteEvent.InputCourseDescription -> {
-                currentState.copy(isCourseDescriptionMaxInputted = event.text.length == COURSE_DESCRIPTION_MAX_LENGTH)
+
+            is CourseWriteAction.InputCourseDescription -> {
+                flowOf(
+                    CourseWriteMutation.Mutation.UpdateCourseDescription(
+                        isCourseDescriptionMaxInputted = action.text.length == COURSE_DESCRIPTION_MAX_LENGTH
+                    )
+                )
             }
-            is CourseWriteEvent.SelectDate -> {
-                currentState.copy(date = event.date)
+
+            is CourseWriteAction.SelectDate -> {
+                flowOf(
+                    CourseWriteMutation.Mutation.UpdateCourseDate(date = action.date)
+                )
             }
-            is CourseWriteEvent.SelectImages -> {
-                val newPlaces = currentState.places.map { place ->
-                    if (place.order == event.placeOrder) {
-                        val newImages = (place.images + event.images.map { CoursePlaceImage(origin = it) }).take(PLACE_IMAGE_MAX_COUNT)
+
+            is CourseWriteAction.SelectImages -> {
+                val newPlaces = state.places.map { place ->
+                    if (place.order == action.placeOrder) {
+                        val newImages =
+                            (place.images + action.images.map { CoursePlaceImage(origin = it) }).take(
+                                PLACE_IMAGE_MAX_COUNT
+                            )
                         place.copy(images = newImages)
                     } else {
                         place
                     }
                 }
-                currentState.copy(places = newPlaces)
+                flowOf(
+                    CourseWriteMutation.Mutation.AddPlaceImages(places = newPlaces)
+                )
             }
-            is CourseWriteEvent.ClickDeletePlace -> {
-                val newPlaces = currentState.places.filterNot { place -> place.order == event.placeOrder }
-                currentState.copy(places = newPlaces)
+
+            is CourseWriteAction.ClickDeletePlace -> {
+                val newPlaces = state.places.filterNot { place -> place.order == action.placeOrder }
+                flowOf(
+                    CourseWriteMutation.Mutation.DeletePlace(places = newPlaces)
+                )
             }
-            is CourseWriteEvent.ClickDeletePlaceImage -> {
-                val newPlaces = currentState.places.map { place ->
-                    if (place.order == event.placeOrder) {
-                        val newImages = place.images.filterIndexed { index, _ -> index != event.imageIndex }
+
+            is CourseWriteAction.ClickDeletePlaceImage -> {
+                val newPlaces = state.places.map { place ->
+                    if (place.order == action.placeOrder) {
+                        val newImages =
+                            place.images.filterIndexed { index, _ -> index != action.imageIndex }
                         place.copy(images = newImages)
                     } else {
                         place
                     }
                 }
-                currentState.copy(places = newPlaces)
+                flowOf(
+                    CourseWriteMutation.Mutation.DeletePlaceImage(places = newPlaces)
+                )
             }
-            is CourseWriteEvent.InputPlaceReview -> {
-                val newPlaces = currentState.places.map { place ->
-                    if (place.order == event.placeOrder) {
-                        place.copy(isPlaceReviewMaxInputted = event.text.length == PLACE_REVIEW_MAX_LENGTH)
+
+            is CourseWriteAction.InputPlaceReview -> {
+                val newPlaces = state.places.map { place ->
+                    if (place.order == action.placeOrder) {
+                        place.copy(isPlaceReviewMaxInputted = action.text.length == PLACE_REVIEW_MAX_LENGTH)
                     } else {
                         place
                     }
                 }
-                currentState.copy(places = newPlaces)
+                flowOf(
+                    CourseWriteMutation.Mutation.UpdatePlaceReview(places = newPlaces)
+                )
             }
-            is CourseWriteEvent.ToggleVisibilitySwitch -> {
-                currentState.copy(isPrivate = event.checked)
-            }
-            else -> {
-                currentState
-            }
-        }
-    }
 
-    fun handleSideEffect(
-        currentState: CourseWriteState,
-        event: CourseWriteEvent
-    ): CourseWriteSideEffect {
-        return when (event) {
-            CourseWriteEvent.ClickDatePicker -> {
-                CourseWriteSideEffect.ShowDatePickerDialog
+            is CourseWriteAction.ToggleVisibilitySwitch -> {
+                flowOf(
+                    CourseWriteMutation.Mutation.UpdateCourseVisibility(isPrivate = action.checked)
+                )
             }
-            CourseWriteEvent.ClickAddPlace -> {
-                if (currentState.places.size == COURSE_PLACE_MAX_COUNT) {
-                    CourseWriteSideEffect.ShowSnackBar("TODO")
+
+            CourseWriteAction.ClickDatePicker -> {
+                flowOf(CourseWriteMutation.SideEffect.ShowDatePickerDialog)
+            }
+
+            CourseWriteAction.ClickAddPlace -> {
+                if (state.places.size == COURSE_PLACE_MAX_COUNT) {
+                    flowOf(CourseWriteMutation.SideEffect.ShowSnackBar("TODO"))
                 } else {
-                    CourseWriteSideEffect.None
+                    flowOf(CourseWriteMutation.SideEffect.GoToAddPlace)
                 }
             }
-            is CourseWriteEvent.ClickAddPlaceImage -> {
-                val currentImagesCount = currentState.places.first { it.order == event.placeOrder }.images.size
+
+            is CourseWriteAction.ClickAddPlaceImage -> {
+                val currentImagesCount =
+                    state.places.first { it.order == action.placeOrder }.images.size
                 if (currentImagesCount == PLACE_IMAGE_MAX_COUNT) {
-                    CourseWriteSideEffect.ShowSnackBar("TODO")
+                    flowOf(CourseWriteMutation.SideEffect.ShowSnackBar("TODO"))
                 } else {
-                    CourseWriteSideEffect.None
+                    val remainCount = PLACE_IMAGE_MAX_COUNT - currentImagesCount
+                    flowOf(
+                        CourseWriteMutation.SideEffect.ShowPhotoPicker(remainCount = remainCount)
+                    )
                 }
             }
-            CourseWriteEvent.StartApiCall -> {
-                CourseWriteSideEffect.ShowLoading
-            }
-            CourseWriteEvent.CompleteApiCall -> {
-                CourseWriteSideEffect.HideLoading
-            }
-            is CourseWriteEvent.UploadCourse -> {
-                when (event.response) {
-                    is Response.Fail -> CourseWriteSideEffect.ShowSnackBar(event.response.message)
-                    is Response.Success -> CourseWriteSideEffect.Finish
+
+            is CourseWriteAction.ClickUploadButton -> {
+                val result = when (val response = getImageUploadUrlUseCase("", "")) {
+                    is Response.Fail -> CourseWriteMutation.SideEffect.ShowSnackBar(response.message)
+                    is Response.Success -> CourseWriteMutation.SideEffect.Finish
                 }
+
+                flowOf(
+                    CourseWriteMutation.SideEffect.ShowLoading,
+                    result,
+                    CourseWriteMutation.SideEffect.HideLoading,
+                )
             }
-            else -> CourseWriteSideEffect.None
         }
     }
 
     companion object {
         private const val COURSE_PLACE_MAX_COUNT = 8
-        private const val COURSE_TITLE_MAX_LENGTH = 40
+        private const val COURSE_TITLE_MAX_LENGTH = 30
         private const val COURSE_DESCRIPTION_MAX_LENGTH = 300
         private const val PLACE_REVIEW_MAX_LENGTH = 300
         private const val PLACE_IMAGE_MAX_COUNT = 4
