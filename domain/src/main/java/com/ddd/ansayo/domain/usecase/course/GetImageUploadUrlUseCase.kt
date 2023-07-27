1f90564fc5c9ac1a0f713a5e14d05a6c8b187215
Yoonsang Kwak <yunsang08@naver.com>
2023-07-27 21:33:50 +0900

    [ feature ] : 코스 작성 - Domain 설계 변경 (#17)
    * [ feature ] : Event -> Mutation, SideEffect로 설계 변경
    
    * [ feature ] : handler 로직 변경
    
    * [ feature ] : handler 로직 변경으로 인한 viewModel 코드 수정
    
    * [ feature ] : app 모듈에서 domain 모델과 orbit 사용할수있도록 수정
    
    * [ feature ] : eventHandler 네이밍 변경

--- domain/src/main/java/com/ddd/ansayo/domain/usecase/course/GetImageUploadUrlUseCase.kt
+++ domain/src/main/java/com/ddd/ansayo/domain/usecase/course/GetImageUploadUrlUseCase.kt
@@ -3,18 +3,13 @@ package com.ddd.ansayo.domain.usecase.course
 import com.ddd.ansayo.domain.entity.course.UploadImageUrlEntity
 import com.ddd.ansayo.domain.model.common.Response
 import com.ddd.ansayo.domain.repository.CourseRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetImageUploadUrlUseCase @Inject constructor(
     private val courseRepository: CourseRepository
 ) {
 
-    operator fun invoke(code: String, fileName: String): Flow<Response<UploadImageUrlEntity>> {
-        return flow {
-            val response = courseRepository.getUploadImageUrl(code, fileName)
-            emit(response)
-        }
+    suspend operator fun invoke(code: String, fileName: String): Response<UploadImageUrlEntity> {
+        return courseRepository.getUploadImageUrl(code, fileName)
     }
 }
