5e0a7a7e9039c97ff3333ae924d4f2991b1a83c7
yoonsangkwak <yunsang08@naver.com>
2023-08-09 21:24:39 +0900

    [ feature] : 관심 목록 - App layer

--- app/src/main/java/com/ddd/ansayo/main/MainActivity.kt
+++ app/src/main/java/com/ddd/ansayo/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.ddd.ansayo.main
 
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import com.ddd.ansayo.R
 import com.ddd.ansayo.base.BaseActivity
 import com.ddd.ansayo.databinding.ActivityMainBinding
+import com.ddd.ansayo.presentation.viewmodel.Constant
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,10 +25,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
         binding.bottomNavigation.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.menu_home -> binding.vpFragment.currentItem = MainFragmentAdapter.HOME
-                R.id.menu_my_record -> binding.vpFragment.currentItem = MainFragmentAdapter.MY_RECORD
-                R.id.menu_favorite -> binding.vpFragment.currentItem = MainFragmentAdapter.FAVORITE
-                R.id.menu_my_page -> binding.vpFragment.currentItem = MainFragmentAdapter.MY_PAGE
+                R.id.menu_home -> {
+                    binding.vpFragment.setCurrentItem(MainFragmentAdapter.HOME, false)
+                }
+                R.id.menu_my_record -> {
+                    binding.vpFragment.setCurrentItem(MainFragmentAdapter.MY_RECORD, false)
+                }
+                R.id.menu_favorite -> {
+                    binding.vpFragment.setCurrentItem(MainFragmentAdapter.FAVORITE, false)
+                    supportFragmentManager.setFragmentResult(Constant.SELECT_FAVORITE_TAB, bundleOf())
+                }
+                R.id.menu_my_page -> {
+                    binding.vpFragment.setCurrentItem(MainFragmentAdapter.MY_PAGE, false)
+                }
             }
             true
         }
