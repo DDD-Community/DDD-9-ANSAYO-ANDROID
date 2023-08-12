180aee9b7312eb8a2dc7a8b688426215ffc2e0d6
Yoonsang Kwak <yunsang08@naver.com>
2023-08-12 14:34:17 +0900

    [ feature ] : 관심 목록 - App Layer (#41)
    * [ feature ] : 리소스 추가
    
    * [ feature ] : 관심 목록 코스 - App layer
    
    * [ feature ] : 관심 목록 장소 - App layer
    
    * [ feature] : 관심 목록 - App layer

---
+++ app/src/main/java/com/ddd/ansayo/main/favorite/FavoriteCourseAdapter.kt
@@ -0,0 +1,52 @@
+package com.ddd.ansayo.main.favorite
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ddd.ansayo.databinding.ItemFavoriteCourseBinding
+import com.ddd.ansayo.domain.model.course.Course
+import com.ddd.ansayo.util.ItemCallback
+
+class FavoriteCourseAdapter(
+    private val courseClickListener: (String) -> Unit,
+    private val favoriteClickListener: (String, Boolean) -> Unit
+) : ListAdapter<Course, FavoriteCourseAdapter.ViewHolder>(
+    ItemCallback(
+        itemTheSame = { oldItem, newItem -> oldItem == newItem },
+        contentsTheSame = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemFavoriteCourseBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding, courseClickListener, favoriteClickListener)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    class ViewHolder(
+        private val binding: ItemFavoriteCourseBinding,
+        private val courseClickListener: (String) -> Unit,
+        private val favoriteClickListener: (String, Boolean) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                binding.course?.let { courseClickListener(it.toString()) }
+            }
+
+            binding.ivFavorite.setOnClickListener {
+                binding.course?.let {
+                    favoriteClickListener(it.toString(), binding.ivFavorite.isSelected)
+                }
+            }
+        }
+
+        fun onBind(item: Course) {
+            binding.course = item
+        }
+    }
+}
---
+++ app/src/main/java/com/ddd/ansayo/main/favorite/FavoritePlaceFragment.kt
@@ -0,0 +1,118 @@
+package com.ddd.ansayo.main.favorite
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ddd.ansayo.R
+import com.ddd.ansayo.base.BaseFragment
+import com.ddd.ansayo.core_design.util.DividerDecoration
+import com.ddd.ansayo.databinding.FragmentFavoritePlaceBinding
+import com.ddd.ansayo.domain.model.favorite.FavoritePlaceAction
+import com.ddd.ansayo.domain.model.favorite.FavoritePlaceMutation
+import com.ddd.ansayo.extenstion.setHtmlText
+import com.ddd.ansayo.presentation.viewmodel.Constant
+import com.ddd.ansayo.presentation.viewmodel.favorite.FavoritePlaceViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class FavoritePlaceFragment :
+    BaseFragment<FragmentFavoritePlaceBinding>(FragmentFavoritePlaceBinding::inflate) {
+
+    private val viewModel by viewModels<FavoritePlaceViewModel>()
+    private val favoriteAdapter = FavoritePlaceAdapter(
+        placeClickListener = {
+            viewModel.onAction(FavoritePlaceAction.ClickPlace(it))
+        },
+        favoriteClickListener = { id, like ->
+            viewModel.onAction(FavoritePlaceAction.ClickFavorite(id, like))
+        }
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        setFragmentResultListener()
+        collectState()
+        collectSideEffect()
+    }
+
+    private fun initView() {
+        binding.tvNoFavoriteTitle.setHtmlText(getString(R.string.no_favorite_place_title))
+        binding.btnFindPlace.setButtonText(getString(R.string.find_place))
+
+        binding.btnFindPlace.setOnClickListener {
+            viewModel.onAction(FavoritePlaceAction.ClickFindPlace)
+        }
+
+        binding.rvFavoritePlace.apply {
+            adapter = favoriteAdapter
+            layoutManager = LinearLayoutManager(context)
+            itemAnimator = null
+            setHasFixedSize(true)
+            addItemDecoration(DividerDecoration(context, 16))
+        }
+    }
+
+    private fun setFragmentResultListener() {
+        setFragmentResultListener(Constant.FETCH_FAVORITE_PLACE) { _, _ ->
+            viewModel.onAction(FavoritePlaceAction.SelectFavoriteListTab)
+        }
+    }
+
+    private fun collectState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                launch {
+                    viewModel.container.stateFlow
+                        .map { it.places }
+                        .distinctUntilChanged()
+                        .collect {
+                            favoriteAdapter.submitList(it)
+                        }
+                }
+
+                launch {
+                    viewModel.container.stateFlow
+                        .map { it.hasFavorites }
+                        .distinctUntilChanged()
+                        .collect {
+                            binding.layoutNoFavorites.isVisible = it.not()
+                        }
+                }
+            }
+        }
+    }
+
+    private fun collectSideEffect() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.container.sideEffectFlow.collect {
+                    when (it) {
+                        is FavoritePlaceMutation.SideEffect.NaviToPlaceDetail -> {
+
+                        }
+
+                        FavoritePlaceMutation.SideEffect.NaviToSearch -> {
+
+                        }
+
+                        is FavoritePlaceMutation.SideEffect.ShowSnackBar -> {
+                            Snackbar.make(binding.root, it.message, Snackbar.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
