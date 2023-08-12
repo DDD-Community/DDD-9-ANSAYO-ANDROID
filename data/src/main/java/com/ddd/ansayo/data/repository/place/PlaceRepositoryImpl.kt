4b890d27eefd39f2a75777a021402841fcd28a91
yoonsangkwak <yunsang08@naver.com>
2023-08-12 16:03:27 +0900

    [ feature ] : datasource 및 service 추가

--- data/src/main/java/com/ddd/ansayo/data/repository/place/PlaceRepositoryImpl.kt
+++ data/src/main/java/com/ddd/ansayo/data/repository/place/PlaceRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.ddd.ansayo.data.repository.place
 
+import com.ddd.ansayo.core_model.place.FavoritePlacesEntity
 import com.ddd.ansayo.core_model.place.Place
 import com.ddd.ansayo.data.datasource.place.PlaceRemoteDataSource
 import com.ddd.ansayo.domain.repository.PlaceRepository
@@ -11,5 +12,17 @@ class PlaceRepositoryImpl @Inject constructor(
 
     override suspend fun getPlaces(query: String): List<Place> {
         return placeRemoteDataSource.getPlaces(query)
+    }
+
+    override suspend fun getFavoritePlaces(): FavoritePlacesEntity.Response {
+        return placeRemoteDataSource.getFavoritePlaces()
+    }
+
+    override suspend fun postFavoritePlace(body: FavoritePlacesEntity.PostRequest) {
+        return placeRemoteDataSource.postFavoritePlace(body)
+    }
+
+    override suspend fun deleteFavoritePlace(placeId: String) {
+        return placeRemoteDataSource.deleteFavoritePlace(placeId)
     }
 }
--- remote/src/main/java/com/ddd/ansayo/remote/service/PlaceService.kt
+++ remote/src/main/java/com/ddd/ansayo/remote/service/PlaceService.kt
@@ -1,8 +1,12 @@
 package com.ddd.ansayo.remote.service
 
+import com.ddd.ansayo.core_model.place.FavoritePlacesEntity
 import com.ddd.ansayo.core_model.place.Place
 import com.skydoves.sandwich.ApiResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface PlaceService {
@@ -11,4 +15,17 @@ interface PlaceService {
     suspend fun getPlaces(
         @Query("query") query: String
     ): ApiResponse<List<Place>>
+
+    @GET("/app/place/favorite")
+    suspend fun getFavoritePlaces(): ApiResponse<FavoritePlacesEntity.Response>
+
+    @POST("/app/place/favorite")
+    suspend fun postFavoritePlace(
+        @Body body: FavoritePlacesEntity.PostRequest
+    ): ApiResponse<Unit>
+
+    @DELETE("/app/place/favorite")
+    suspend fun deleteFavoritePlace(
+        @Query("place_id") placeId: String
+    ): ApiResponse<Unit>
 }
