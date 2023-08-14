67c3971ac409ec8fe650866d595c39f70a09ab1e
yoonsangkwak <yunsang08@naver.com>
2023-08-14 14:19:11 +0900

    [ feature ] : 장소 상세 - data module

--- data/src/main/java/com/ddd/ansayo/data/repository/place/PlaceRepositoryImpl.kt
+++ data/src/main/java/com/ddd/ansayo/data/repository/place/PlaceRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.ddd.ansayo.data.repository.place
 import com.ddd.ansayo.core_model.common.Response
 import com.ddd.ansayo.core_model.place.FavoritePlacesEntity
 import com.ddd.ansayo.core_model.place.Place
+import com.ddd.ansayo.core_model.place.PlaceInfoEntity
 import com.ddd.ansayo.data.datasource.place.PlaceRemoteDataSource
 import com.ddd.ansayo.domain.repository.PlaceRepository
 import javax.inject.Inject
@@ -25,5 +26,12 @@ class PlaceRepositoryImpl @Inject constructor(
 
     override suspend fun deleteFavoritePlace(placeId: String): Response<Unit> {
         return placeRemoteDataSource.deleteFavoritePlace(placeId)
+    }
+
+    override suspend fun getPlaceInfo(
+        placeId: String,
+        courseCount: Int
+    ): Response<PlaceInfoEntity> {
+        return placeRemoteDataSource.getPlaceInfo(placeId, courseCount)
     }
 }
