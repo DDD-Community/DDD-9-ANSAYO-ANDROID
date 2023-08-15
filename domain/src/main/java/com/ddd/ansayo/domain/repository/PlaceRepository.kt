030b51d3f9f909107ea05d9b40b1e3a6c52cffab
Yoonsang Kwak <yunsang08@naver.com>
2023-08-15 22:21:23 +0900

    [ feature ] : 장소 상세 - domain layer (#46)

--- domain/src/main/java/com/ddd/ansayo/domain/repository/PlaceRepository.kt
+++ domain/src/main/java/com/ddd/ansayo/domain/repository/PlaceRepository.kt
@@ -4,6 +4,7 @@ import com.ddd.ansayo.core_model.common.Response
 import com.ddd.ansayo.core_model.place.FavoritePlacesEntity
 import com.ddd.ansayo.core_model.place.Place
 import com.ddd.ansayo.core_model.place.SearchPlaceEntity
+import com.ddd.ansayo.core_model.place.PlaceInfoEntity
 
 interface PlaceRepository {
 
@@ -12,5 +13,5 @@ interface PlaceRepository {
     suspend fun postFavoritePlace(body: FavoritePlacesEntity.PostRequest): Response<Unit>
     suspend fun deleteFavoritePlace(placeId: String): Response<Unit>
     suspend fun getSearchPlaces(query: String): Response<SearchPlaceEntity>
-
+    suspend fun getPlaceInfo(placeId: String, courseCount: Int) : Response<PlaceInfoEntity>
 }
