54dad92535dab98b95150119590daa34e05c0303
Yoonsang Kwak <yunsang08@naver.com>
2023-08-15 22:22:41 +0900

    [ feature ] : 장소 상세 - Presentation layer (#48)
    * [ feature ] : placeDetail 값 nullable로 변경
    
    * [ feature ] : 장소 상세 - Presentation layer

--- domain/src/main/java/com/ddd/ansayo/domain/model/place/PlaceDetailState.kt
+++ domain/src/main/java/com/ddd/ansayo/domain/model/place/PlaceDetailState.kt
@@ -4,9 +4,16 @@ import com.ddd.ansayo.core_model.course.Course
 import com.ddd.ansayo.core_model.place.Place
 
 data class PlaceDetailState(
-    val placeDetail: PlaceDetail,
+    val placeDetail: PlaceDetail?,
     val courses: List<Course>
-)
+) {
+    companion object {
+        val EMPTY = PlaceDetailState(
+            placeDetail = null,
+            courses = emptyList()
+        )
+    }
+}
 
 data class PlaceDetail(
     val place: Place,
