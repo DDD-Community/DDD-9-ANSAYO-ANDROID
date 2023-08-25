c028f83b41ff28a151621ab4f8cb1f78f26a822a
sunyoung <sy.jung@frintrip.com>
2023-08-26 00:04:36 +0900

    CourseInfo AppLayer 수정

--- app/src/main/java/com/ddd/ansayo/course/info/CourseInfoActivity.kt
+++ app/src/main/java/com/ddd/ansayo/course/info/CourseInfoActivity.kt
@@ -1,15 +1,24 @@
 package com.ddd.ansayo.course.info
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.ddd.ansayo.R
 import com.ddd.ansayo.base.BaseActivity
 import com.ddd.ansayo.databinding.ActivityCourseInfoBinding
 import com.ddd.ansayo.domain.model.course.info.CourseInfoAction
 import com.ddd.ansayo.domain.model.course.info.CourseInfoMutation
+import com.ddd.ansayo.presentation.viewmodel.Constant
 import com.ddd.ansayo.presentation.viewmodel.course.CourseInfoViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.LatLngBounds
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
@@ -25,18 +34,57 @@ class CourseInfoActivity :
             viewModel.onAction(CourseInfoAction.NaviToPlaceDetail(placeId))
         }
     )
-
+    lateinit var mapFragment: MapFragment
+    lateinit var naverMap: NaverMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initView()
         collectState()
         collectSideEffect()
+
+        val courseId = intent.getStringExtra(Constant.COURSE_ID).orEmpty()
+        viewModel.onAction(CourseInfoAction.EnteredScreen(courseId))
     }
 
     private fun initView() {
         binding.rvCoursePlave.apply {
             adapter = courseInfoAdapter
+        }
+    }
+
+    private fun getMapLocation() {
+        val builder = LatLngBounds.Builder()
+
+//        for (marker in markerList) {
+//            builder.include(marker.position)
+//        }
+
+        val bounds = builder.build()
+
+//        val padding = resources.getDimensionPixelSize(R.dimen.map_padding)
+//        val cameraUpdate = CameraUpdate.fitBounds(bounds, padding)
+//        naverMap.moveCamera(cameraUpdate)
+    }
+
+    private fun createMapFragment(courseLocation: LatLng) {
+        mapFragment = createNaverMapFragment(courseLocation)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_map_container, mapFragment)
+            .commitAllowingStateLoss()
+    }
+
+    private fun createNaverMapFragment(courseLocation: LatLng): MapFragment {
+        return MapFragment.newInstance().apply {
+            getMapAsync {
+                it.minZoom = 11.5
+                it.uiSettings.apply {
+                    isCompassEnabled = false
+                    isZoomControlEnabled = true
+                    isLocationButtonEnabled = false
+                }
+                naverMap = it
+            }
         }
     }
     private fun collectState() {
@@ -66,13 +114,20 @@ class CourseInfoActivity :
                         is CourseInfoMutation.SideEffect.NaviToPlaceDetail -> {
 
                         }
+                        is CourseInfoMutation.SideEffect.ShowSnackBar -> {
+                            Snackbar.make(binding.root, it.message, Snackbar.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
 
         }
-
     }
-
-
+    companion object {
+        fun getIntent(context: Context, courseId: String): Intent {
+            return Intent(context, CourseInfoActivity::class.java).apply {
+                putExtra(Constant.COURSE_ID, courseId)
+            }
+        }
+    }
 }
--- app/src/main/java/com/ddd/ansayo/login/LoginActivity.kt
+++ app/src/main/java/com/ddd/ansayo/login/LoginActivity.kt
@@ -33,7 +33,9 @@ class LoginActivity:
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
+        startActivity(Intent(this@LoginActivity, CourseInfoActivity::class.java))
+        Logger.e("token ${authLocalDataSource.authToken}")
         initView()
         collectSideEffect()
     }
