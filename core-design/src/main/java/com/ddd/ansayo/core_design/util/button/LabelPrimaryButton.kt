20c457132b5bf4c67a9ab93cfbe2db2384ebe3cd
yoonsangkwak <yunsang08@naver.com>
2023-08-26 20:16:50 +0900

    [ refactor ] : LabelPrimaryButton 커스텀뷰 수정

--- core-design/src/main/java/com/ddd/ansayo/core_design/util/button/LabelPrimaryButton.kt
+++ core-design/src/main/java/com/ddd/ansayo/core_design/util/button/LabelPrimaryButton.kt
@@ -2,50 +2,25 @@ package com.ddd.ansayo.core_design.util.button
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import androidx.annotation.StringRes
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
 import com.ddd.ansayo.core_design.R
-import com.ddd.ansayo.core_design.databinding.DsButtonPrimaryDefaultBinding
 
 class LabelPrimaryButton @JvmOverloads constructor(
     context: Context,
-    attributeSet: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : ConstraintLayout(context, attributeSet, defStyleAttr) {
+    attributeSet: AttributeSet,
+    defStyleAttr: Int = 0,
+) : AppCompatButton(context, attributeSet, defStyleAttr) {
 
-    private val binding =
-        DsButtonPrimaryDefaultBinding.inflate(LayoutInflater.from(context), this, true).apply {
-            view = this@LabelPrimaryButton
-            executePendingBindings()
-        }
-
-    private var clickEvent: (() -> Unit)? = null
-
-    fun setOnClicked(event: () -> Unit) {
-        clickEvent = event
-    }
-
-    fun onClicked() {
-        clickEvent?.invoke()
-    }
-
-    fun setButtonText(text: String) {
-        binding.tvContent.text = text
-    }
-
-    fun setButtonText(@StringRes textRes: Int) {
-        binding.tvContent.text = context.getString(textRes)
-    }
-
-    override fun setEnabled(isEnabled: Boolean) {
-        binding.root.isEnabled = isEnabled
-        binding.tvContent.setBackgroundColor(
-            ContextCompat.getColor(
-                context,
-                if (isEnabled) R.color.orange_point else R.color.N80
-            )
-        )
+    init {
+        val attrs = context.obtainStyledAttributes(attributeSet, R.styleable.LabelPrimaryButton)
+        background = ContextCompat.getDrawable(context, R.drawable.bg_button_label_primary_default)
+        gravity = Gravity.CENTER
+        setTextAppearance(R.style.Text_L1)
+        text = attrs.getString(R.styleable.LabelPrimaryButton_android_text)
+        setTextColor(ContextCompat.getColor(context, R.color.N100))
+        isEnabled = attrs.getBoolean(R.styleable.LabelPrimaryButton_android_enabled, false)
+        attrs.recycle()
     }
 }
