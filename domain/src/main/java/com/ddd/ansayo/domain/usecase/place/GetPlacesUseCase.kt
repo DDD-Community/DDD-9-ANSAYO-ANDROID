bef43b8286358247a5c973f2372337fe74d2733b
Yoonsang Kwak <yunsang08@naver.com>
2023-08-29 20:55:13 +0900

    [ feature ] : 코스 작성하기 - 장소 추가하기 UI 변경 (#70)

--- domain/src/main/java/com/ddd/ansayo/domain/usecase/place/GetPlacesUseCase.kt
+++ domain/src/main/java/com/ddd/ansayo/domain/usecase/place/GetPlacesUseCase.kt
@@ -2,6 +2,7 @@ package com.ddd.ansayo.domain.usecase.place
 
 import com.ddd.ansayo.core_model.common.Response
 import com.ddd.ansayo.core_model.place.Place
+import com.ddd.ansayo.core_model.place.SearchPlaceEntity
 import com.ddd.ansayo.domain.repository.PlaceRepository
 import javax.inject.Inject
 
@@ -9,7 +10,7 @@ class GetPlacesUseCase @Inject constructor(
     private val placeRepository: PlaceRepository
 ) {
 
-    suspend operator fun invoke(query: String): Response<List<Place>> {
-        return placeRepository.getPlaces(query)
+    suspend operator fun invoke(query: String): Response<SearchPlaceEntity> {
+        return placeRepository.getSearchPlaces(query)
     }
 }
